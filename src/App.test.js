c5258a35ed3742343f4002d408508d33755552ca
Jack Song <133342301@163.com>
2020-03-25 01:54:22 +1100

    Configured Snapshot Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import initStoryshots from '@storybook/addon-storyshots';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -7,3 +8,5 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+initStoryshots();
